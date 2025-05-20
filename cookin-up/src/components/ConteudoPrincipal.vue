2ac6b2177eb8fa085aa8f00fa86fe9acee87b4b2
Guilherme Taira <guilherme.taira@onr.org.br>
2025-05-20 12:32:45 -0300

    renderizando componente condicionalmente

--- cookin-up/src/components/ConteudoPrincipal.vue
+++ cookin-up/src/components/ConteudoPrincipal.vue
@@ -13,11 +13,16 @@ export default {
   <main class="conteudo-principal">
     <section>
       <span class="subtitulo-lg sua-lista-texto">Sua lista:</span>
-      <ul class="ingredientes-sua-lista">
+      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
         <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
           {{ ingrediente }}
         </li>
       </ul>
+
+      <p v-else class="paragrafo lista-vazia">
+        <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa" />
+        Sua lista está vazia, selecione ingredientes para iniciar!
+      </p>
     </section>
   </main>
 </template>
