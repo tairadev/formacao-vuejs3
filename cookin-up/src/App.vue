6d8a6d1cf709eec5b928a864cd8eea1f72c541cf
Guilherme Taira <guilherme.taira@onr.org.br>
2025-05-20 12:30:24 -0300

    repetindo itens com v-for

--- cookin-up/src/App.vue
+++ cookin-up/src/App.vue
@@ -1,14 +1,17 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
   name: 'App',
   components: {
     Banner,
+    ConteudoPrincipal,
   },
 }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
