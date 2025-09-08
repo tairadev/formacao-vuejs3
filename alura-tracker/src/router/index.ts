378d0ff9d4f95efdb252f716d19b980fd7710ada
Guilherme Taira <guilherme.taira@onr.org.br>
2025-09-08 13:21:19 -0300

    rotas aninhadas

--- alura-tracker/src/router/index.ts
+++ alura-tracker/src/router/index.ts
@@ -8,19 +8,25 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: "/projetos",
-    name: "Projetos",
     component: () => import("@/views/Projetos.vue"),
-  },
-  {
-    path: "/projetos/novo",
-    name: "NovoProjeto",
-    component: () => import("@/views/Projetos/Formulario.vue"),
-  },
-  {
-    path: "/projetos/:id",
-    name: "ProjetoDetalhe",
-    component: () => import("@/views/Projetos/Formulario.vue"),
-    props: true,
+    children: [
+      {
+        path: "",
+        name: "ListaProjetos",
+        component: () => import("@/views/Projetos/Lista.vue"),
+      },
+      {
+        path: "novo",
+        name: "NovoProjeto",
+        component: () => import("@/views/Projetos/Formulario.vue"),
+      },
+      {
+        path: ":id",
+        name: "ProjetoDetalhe",
+        component: () => import("@/views/Projetos/Formulario.vue"),
+        props: true,
+      },
+    ],
   },
 ];
 
--- alura-tracker/src/views/Projetos.vue
+++ alura-tracker/src/views/Projetos.vue
@@ -1,49 +1,15 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-    <router-link to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <div class="fas fa-plus"></div>
-      </span>
-      <span>Novo Projeto</span>
-    </router-link>
-    <table class="table is-fullwidth mt-3">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useStore } from "@/store";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ProjetosView",
-  setup() {
-    const store = useStore();
-    return {
-      projetos: computed(() => store.state.projetos),
-    };
-  },
 });
 </script>
 
